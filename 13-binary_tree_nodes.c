004fed78b04a2e41999e420cf56d270ca58b6091
Kevin Kariuki <vingentz@gmail.com>
2023-08-29 15:19:02 +0300

    Task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts nodes with at least one child
+ * @tree: pointer to the root node
+ * Return: 0 if tree is NULL
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (binary_tree_nodes(tree->left) +
+				binary_tree_nodes(tree->right) + 1);
+	}
+	return (0);
+}
