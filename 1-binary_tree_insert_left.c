88cac039ad2c6167eaa1abce474296c1f4edd790
Kevin Kariuki <vingentz@gmail.com>
2023-08-29 14:00:38 +0300

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = new_left_node;
 	}
 	parent->left = new_left_node;
-	
+
 	return (new_left_node);
 }
